b295148c79802867a5cb5d2390cc8d7c4932bd92
Miguel Curbelo <mcurbeloj@gmail.com>
2022-10-05 18:32:24 +0200

    Solves bonues iteration 9: centiseconds

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval (() => {
+      this. currentTime += 1;
+      if (printTimeCallback){
+        printTimeCallback()
+      }
+    }, 10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/100)%60);
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime%100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    return ('0'+value).slice(-2);
+}
 
-  stop() {
-    // ... your code goes here
-  }
+stop() {
+  clearInterval(this.intervalId);
+}
 
-  reset() {
-    // ... your code goes here
-  }
+reset() {
+  this.currentTime = 0;
+}
 
-  split() {
-    // ... your code goes here
-  }
+split() {
+  const splitMinutes = this.computeTwoDigitNumber(this.getMinutes());
+  const splitSeconds = this.computeTwoDigitNumber(this.getSeconds());
+  const splitCentiseconds = this.computeTwoDigitNumber(this.getCentiseconds());
+  return `${splitMinutes}:${splitSeconds}.${splitCentiseconds}`; 
+}
 }
